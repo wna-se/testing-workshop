815b00de5a57413416a05a0adfbdb02820edf2f5
Wolmar Nyberg Åkerström <wolmar.n.akerstrom@nbis.se>
2021-03-17 10:30:39 +0100

    Intentionally failing tests

--- example.py
+++ example.py
@@ -10,7 +10,6 @@ def test_add():
 def subtract(a, b):
     return a + b  # <--- fix this in step 8
 
-
 # uncomment the following test in step 5
-#def test_subtract():
-#    assert subtract(2, 3) == -1
+def test_subtract():
+   assert subtract(2, 3) == -1
